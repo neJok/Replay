544184ae53ef607afc0287b94b788d61dc685285
neJok <uk6bobh@proton.me>
2024-09-04 23:13:08 +0300

    add admin

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 MONGODB_TOKEN = os.getenv('MONGODB_TOKEN')
 
-ADMINS = [588093669, 956945958]
+ADMINS = [588093669, 956945958, 6252741731]
 
 client = AsyncIOMotorClient(MONGODB_TOKEN)
 db = client.replay
